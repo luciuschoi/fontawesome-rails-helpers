745a4ed7a4415b412337600a1aff594384047966
Lucius Choi <lucius.choi@gmail.com>
2021-09-20 08:21:20 +0900

    initial commit

---
+++ lib/fontawesome/rails/helpers/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Fontawesome
+  module Rails
+    module Helpers
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/fontawesome/rails/helpers_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Fontawesome::Rails::Helpers do
+  it "has a version number" do
+    expect(Fontawesome::Rails::Helpers::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
